41873f07fbd280590b715f8e4cf61e932c5bffbc
Rob Prouse <rob@prouse.org>
2020-08-31 17:08:10 -0400

    Moving the code around so I could add unit tests

--- src/lc3/VirtualMachine.cs
+++ src/lc3/VirtualMachine.cs
@@ -5,11 +5,11 @@ namespace LC3
     public class VirtualMachine
     {
         // 65536 memory locations
-        UInt16[] Memory { get; } = new UInt16[UInt16.MaxValue];
+        public UInt16[] Memory { get; } = new UInt16[UInt16.MaxValue];
 
         // 8 general purpose registers (R0-R7)
         // 1 program counter (PC) register
         // 1 condition flags (COND) register
-        UInt16[] Registers { get; } = new UInt16[(int)LC3.Registers.COUNT];
+        public UInt16[] Registers { get; } = new UInt16[(int)LC3.Registers.COUNT];
     }
 }
