9386752445f6c451f3829e9d7718da701cc645ab
Rob Prouse <rob@prouse.org>
2020-08-31 13:57:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lc3_virtual_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
